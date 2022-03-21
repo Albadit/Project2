e5a4690b0c03862ca17886f3449e73a489d66a51
Albadit <46565849+Albadit@users.noreply.github.com>
2022-03-21 18:42:08 +0100

    new

--- Cinema/Cinema/Movie.cs
+++ Cinema/Cinema/Movie.cs
@@ -11,9 +11,9 @@ namespace Cinema
     class Movie
     {
         public int Id { get; set; } = 0;
-        public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-        public string CreditcardNumber { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public int Age { get; set; } = 0;
 
         public static string JsonFileName() => Path.Combine("data", "accounts.json");
 
@@ -30,13 +30,13 @@ namespace Cinema
         }
         public void Movies()
         {
-            var accounts = Movie.ReadAll();
-            foreach (var account in accounts)
+            var movies = Movie.ReadAll();
+            foreach (var movie in movies)
             {
-                WriteLine($"{account.Id} {account.Email}\n");
-                account.Password = $"reset-{account.Id}";
+                WriteLine($"{movie.Id} {movie.Name}\n");
+                /*movie.Password = $"reset-{movie.Id}";*/
             }
-            Movie.WriteAll(accounts);
+            Movie.WriteAll(movies);
         }
     }
 }
