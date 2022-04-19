3aeb3b04dc7f313fdd10597c885dabc6d0cbc450
tawanharink <99250038+tawanharink@users.noreply.github.com>
2022-04-19 15:31:03 +0200

    Inlog betalen
    Inlog bij Ideal

---
+++ Cinema/Cinema/page/idealLogin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.page
+{
+    class idealLogin
+    {
+        public void idealLoginPage(string filmName)
+        {
+            string title = "Ideal Login Page\n";
+            string[] options = {"Back"};
+            
+            paymentlogin mainMenu = new paymentlogin(title, options);
+            int selectedIndex = mainMenu.Run();
+
+            
+
+            switch (selectedIndex)
+            {
+                case 0:
+                    Ideal myIdeal = new Ideal();
+                    myIdeal.IdealPage(filmName);
+                    break;
+            }
+        }
+    }
+}
