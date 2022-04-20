4ce369bfa5e9dc5840cebb1d3884b0f53de51a75
tawanharink <99250038+tawanharink@users.noreply.github.com>
2022-04-20 10:34:05 +0200

    Registration erbij
    Registration erbij

---
+++ Cinema/Cinema/Registration.cs
@@ -0,0 +1,162 @@
+﻿using Cinema.page;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Cinema
+{
+    class Reservation
+    {
+        private int SelectedIndex;
+        private string[] Options;
+        private string Prompt;
+
+        public Reservation(string title, string[] options)
+        {
+            Prompt = title;
+            Options = options;
+            SelectedIndex = 0;
+        }
+
+        private void Display()
+        {
+
+            WriteLine(Prompt);
+
+            Write("Name: ");
+            var Name = ReadLine();
+
+            while (Name == string.Empty)
+            {
+                WriteLine("Niet leeg laten!");
+                Thread.Sleep(1000);
+                Clear();
+                WriteLine(Prompt);
+                Write("Name: ");
+                Name = ReadLine();
+            }
+
+            Write("Email: ");
+            var Email = ReadLine();
+
+            while (Email == string.Empty)
+            {
+                WriteLine("Niet leeg laten!");
+                Thread.Sleep(1000);
+                Clear();
+                WriteLine(Prompt);
+                WriteLine("Name: " + Name);
+                Write("Email: ");
+                Email = ReadLine();
+            }
+
+            Write("Nummer: ");
+            var Nummer = ReadLine();
+
+            while (Nummer == string.Empty)
+            {
+                WriteLine("Niet leeg laten!");
+                Thread.Sleep(1000);
+                Clear();
+                WriteLine(Prompt);
+                WriteLine("Name: " + Name);
+                WriteLine("Email: " + Email);
+                Write("Nummer: ");
+                Nummer = ReadLine();
+            }
+
+            Write("Leeftijd: ");
+            var Leeftijd = ReadLine();
+
+            /*if (Leeftijd )
+            {
+                Console.WriteLine("x is greater than y");
+            }*/
+
+            while (Leeftijd == string.Empty)
+            {
+                WriteLine("Niet leeg laten!");
+                Thread.Sleep(1000);
+                Clear();
+                WriteLine(Prompt);
+                WriteLine("Name: " + Name);
+                WriteLine("Email: " + Email);
+                Write("Nummer: " + Nummer);
+                Write("\nLeeftijd: ");
+                Leeftijd = ReadLine();
+            }
+
+            WriteLine("\n");
+
+            for (int i = 0; i < Options.Length; i++)
+            {
+                string currentOptions = Options[i];
+
+                if (SelectedIndex == i)
+                {
+                    ForegroundColor = ConsoleColor.Black;
+                    BackgroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    ForegroundColor = ConsoleColor.White;
+                    BackgroundColor = ConsoleColor.Black;
+                }
+
+                if (i == Options.Length - 1)
+                {
+                    WriteLine($" {currentOptions}");
+                }
+                else if (i == Options.Length - 2)
+                {
+                    WriteLine($"{i + 1}) {currentOptions}\n");
+                }
+                else
+                {
+                    WriteLine($"{i + 1}) {currentOptions}");
+                }
+
+            }
+            ResetColor();
+        }
+
+        public int Run()
+        {
+            ConsoleKey keyPressed;
+            do
+            {
+                Clear();
+                Display();
+
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+                keyPressed = keyInfo.Key;
+
+                if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    SelectedIndex--;
+
+                    if (SelectedIndex < 0)
+                    {
+                        SelectedIndex = Options.Length - 1;
+                    }
+                }
+                else if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    SelectedIndex++;
+
+                    if (SelectedIndex > Options.Length - 1)
+                    {
+                        SelectedIndex = 0;
+                    }
+                }
+            }
+            while (keyPressed != ConsoleKey.Enter);
+
+            return SelectedIndex;
+        }
+    }
+}
---
+++ Cinema/Cinema/page/RegistrationPage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.page
+{
+    class RegistrationPage
+    {
+        public void RegistrationPagePage(string filmName)
+        {
+            string title = "Fill in your info.\n";
+            string[] options = { "Payment", "Back" };
+            Reservation mainMenu = new Reservation(title, options);
+            int selectedIndex = mainMenu.Run();
+
+            switch (selectedIndex)
+            {
+                case 0:
+                    Pay myPay = new Pay();
+                    myPay.PayPage(filmName);
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    Seats mySeating = new Seats();
+                    mySeating.SeatingPage(filmName);
+                    break;
+            }
+        }
+    }
+}
