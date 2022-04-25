41dbd6364ef8aa44c0d503653ef7cd724d3f7e2a
Albadit <46565849+Albadit@users.noreply.github.com>
2022-04-26 00:41:15 +0200

    updatte

--- Cinema/Cinema/page/Orders.cs
+++ Cinema/Cinema/page/Orders.cs
@@ -10,18 +10,18 @@ namespace Cinema.page
 {
     class Order
     {
-        public void OrdersPage()
+        public static void OrdersPage()
         {
             List<string> orderList = Product.orderList;
             orderList.Clear();
 
-            Product myOrder = new Product();
+            Product myOrder = new();
             myOrder.Products();
             orderList.Add("Back");
 
             string title = "Choice your film\n";
             string[] options = orderList.ToArray();
-            Menu mainMenu = new Menu(title, options);
+            Menu mainMenu = new(title, options);
             int selectedIndex = mainMenu.Run();
 
             if (selectedIndex == options.Length - 1)
